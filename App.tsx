6fff38e7cb3421861a5626edeb32b3b133158344
Fajar Pamungkas <fajar.pamungkas@Flip-MacBook-M1.local>
2022-08-13 22:41:10 +0700

    :heavy_minus_sign: Remove expo-app-loading due to deprecated libs

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import Navigation from "@/config/navigator";
 import { StatusBar } from "expo-status-bar";
-import AppLoading from "expo-app-loading";
 import { useCachedResources } from "@/hooks";
+import { ActivityIndicator, View } from "react-native";
 
 export default function App() {
   const { isLoadingComplete, fontsLoaded } = useCachedResources();
 
   if (!isLoadingComplete && !fontsLoaded) {
-    return <AppLoading />;
+    return (
+      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
   }
   return (
     <>
